0e58547064eac771fe489cf25a66e214487cb7c6
MHmolesini <Mhmolesini@gmail.com>
2022-03-16 22:37:22 -0300

    Nueva rama, mas creacion del archivo CSS y creacion del sidebar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+// Menu
+let arrow = document.querySelectorAll(".arrow");
+  for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+   arrowParent.classList.toggle("showMenu");
+    });
+  }
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".bx-menu");
+  console.log(sidebarBtn);
+  sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+  });
+
+
 // Se le solicita al usuario que ingrese el nombre de una de las empresas incluidas en el menu desplegable (aun no estan el 100%) y una fecha.
 
 let empresas = prompt("Seleccione una empresa");
